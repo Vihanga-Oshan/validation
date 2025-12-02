ea0e57de5ed8f6de64de097e5b538a8bfd653415
Vihanga Oshan <128878721+Vihanga-Oshan@users.noreply.github.com>
2025-12-02 13:34:54 +0530

    fix: use Cloud Run PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 4001;
+const PORT = process.env.PORT || 4001;
 
 // Parse JSON bodies
 app.use(express.json());
